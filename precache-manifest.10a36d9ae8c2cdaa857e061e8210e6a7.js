b522b25f1a5aef904241c5cc1837546359002c8d
Tom Vandenbroele <vandenbroeletom@gmail.com>
2021-04-25 21:39:45 +0200

    DEPLOY

---
+++ precache-manifest.10a36d9ae8c2cdaa857e061e8210e6a7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329e70db72e24490df91",
+    "url": "/dpm/css/app.cabad472.css"
+  },
+  {
+    "revision": "5a504b7d992989b6dc3ea582e5a4d593",
+    "url": "/dpm/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "5b2b6ab0096b9499179e387ad91338ad",
+    "url": "/dpm/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "b1059e83b0d2e736b889df24c3322766",
+    "url": "/dpm/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d2c3ec839559c9b7c7638c378d45bca5",
+    "url": "/dpm/icons/browserconfig.xml"
+  },
+  {
+    "revision": "d3afac3550a65157d79bc0f8dfe14321",
+    "url": "/dpm/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "32052058fa879fd3455e10a229311b56",
+    "url": "/dpm/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "eab0216bba500f4409d49bda6d5e24bc",
+    "url": "/dpm/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "a5ad90c6ce016a096f235fc12aeed526",
+    "url": "/dpm/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "b029b36da21458319debcfb2786f2886",
+    "url": "/dpm/icons/site.webmanifest"
+  },
+  {
+    "revision": "ab1294bc7e9b0f50261415e68ebbdd51",
+    "url": "/dpm/index.html"
+  },
+  {
+    "revision": "329e70db72e24490df91",
+    "url": "/dpm/js/app.de4bc2c9.js"
+  },
+  {
+    "revision": "dfd36cebc94b7237c94c",
+    "url": "/dpm/js/chunk-vendors.0ab3b853.js"
+  },
+  {
+    "revision": "9f1ad25c072a0b129e92dec83665a034",
+    "url": "/dpm/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dpm/robots.txt"
+  }
+]);
